e64778c0048f1338afa9425feb308577b94cc317
a.falei <august3@mail.ru>
2023-12-07 20:48:24 +0300

    task16

---
+++ next-tasks/src/app/task16/Calculator.js
@@ -0,0 +1,6 @@
+
+
+export default function Calculator(str) {
+
+    return eval(str)
+}
--- next-tasks/src/app/task16/page.js
+++ next-tasks/src/app/task16/page.js
@@ -1,8 +1,11 @@
+'use client'
 import Link from "next/link";
-
-
+import { useState } from "react";
+import Calculator from "./Calculator";
+import style from './style.module.css'
 
 export default function Task16() {
+    const [value, setValue] = useState('')
 
     return (
         <>
@@ -12,8 +15,31 @@ export default function Task16() {
                 компонента. page.js: Содержит калькулятор. Calculator.js: Принимает введенные
                 данные и выполняет математические операции для расчета итогового результата.
             </p>
-            <input type="text" />
-            <input type="text" />
+
+           
+            <div className={style.cont}>
+                <input type="text" value={value} />
+                <div className={style.contWR}>
+                    <input type="button" value={0} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={2} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={3} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={4} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={5} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={6} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={7} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={8} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={9} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={'+'} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={'*'} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={'/'} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={'-'} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={'.'} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"="} onClick={e => setValue(Calculator(value))} />
+                    <input type="button" value={"DE"} onClick={e => setValue(Calculator(value.slice(0, -1)))} />
+                    <input type="button" value={"AC"} onClick={e => setValue('')} />
+                </div>
+            </div>
+
             <p>
                 <Link href='/'>back</Link>
             </p>
