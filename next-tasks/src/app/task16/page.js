971d73bf9207da23127bdbf3f4de32e2ea0ad989
a.falei <august3@mail.ru>
2023-12-12 23:39:06 +0300

    task16 use map

--- next-tasks/src/app/task16/page.js
+++ next-tasks/src/app/task16/page.js
@@ -6,7 +6,10 @@ import style from './style.module.css'
 
 export default function Task16() {
     const [value, setValue] = useState('')
-
+    const arr1 = [0, 1, 2, 3];
+    const arr2 = [4, 5, 6, 7];
+    const arr3 = [8, 9, '+', '*'];
+    const arr4 = ['/', '-', '.', '00'];
     return (
         <>
             <p>
@@ -16,7 +19,6 @@ export default function Task16() {
                 данные и выполняет математические операции для расчета итогового результата.
             </p>
 
-
             <div className={style.container}>
                 <form action="">
                     <div className={style.display}>
@@ -24,28 +26,16 @@ export default function Task16() {
                     </div>
                     <div className={style.calculator}>
                         <div>
-                            <input type="button" value={0} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={1} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={2} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={3} onClick={e => setValue(value + e.target.value)} />
+                            {arr1.map((el) => <input type="button" value={el} onClick={e => setValue(value + e.target.value)} />)}
                         </div>
                         <div>
-                            <input type="button" value={4} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={5} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={6} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={7} onClick={e => setValue(value + e.target.value)} />
+                            {arr2.map((el) => <input type="button" value={el} onClick={e => setValue(value + e.target.value)} />)}
                         </div>
                         <div>
-                            <input type="button" value={8} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={9} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={'+'} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={'*'} onClick={e => setValue(value + e.target.value)} />
+                            {arr3.map((el) => <input type="button" value={el} onClick={e => setValue(value + e.target.value)} />)}
                         </div>
                         <div>
-                            <input type="button" value={'/'} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={'-'} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={'.'} onClick={e => setValue(value + e.target.value)} />
-                            <input type="button" value={'00'} onClick={e => setValue(value + e.target.value)} />
+                            {arr4.map((el) => <input type="button" value={el} onClick={e => setValue(value + e.target.value)} />)}
                         </div>
                         <div>
                             <input type="button" value={"DE"} onClick={e => setValue(Calculator(value.slice(0, -1)))} />
